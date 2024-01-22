9df6948478699a65d45bd92242de7647288d4624
noahtq <vfx.noaht@gmail.com>
2024-01-22 14:23:14 -0600

    Add view, template, and url for player's list of past rounds

--- golftracker/rounds/urls.py
+++ golftracker/rounds/urls.py
@@ -5,4 +5,5 @@ app_name = "rounds"
 urlpatterns = [
     path('', views.welcome, name='dashboard'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('library/', views.RoundListView.as_view(), name='library')
 ]
--- golftracker/rounds/views.py
+++ golftracker/rounds/views.py
@@ -1,13 +1,26 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
 from .models import Round
 
 
 def welcome(request):
     return render(request, 'rounds/welcome.html')
 
+
 @login_required
 def dashboard(request):
     rounds = Round.objects.all()
     context = {"rounds": rounds}
     return render(request, 'rounds/dashboard.html', context)
+
+
+class RoundListView(LoginRequiredMixin, generic.ListView):
+    template_name = 'rounds/roundlist.html'
+    context_object_name = 'rounds_list'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Round.objects.filter(player=self.request.user).order_by('-datetime')[:100]
