403ec310fb2c2d23ed9873c3af1913a83edf0562
noahtq <vfx.noaht@gmail.com>
2024-01-22 08:26:18 -0600

    Create User app and create user registration page

--- golftracker/golftracker/settings.py
+++ golftracker/golftracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'courselibrary.apps.CourselibraryConfig',
     'rounds.apps.RoundsConfig',
     'django.contrib.admin',
--- golftracker/golftracker/urls.py
+++ golftracker/golftracker/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('', include('rounds.urls')),
     path('courselibrary/', include('courselibrary.urls')),
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
 ]
