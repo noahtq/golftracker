43ce28008eeec656b550d4b3f12c4e75fd091ea6
noahtq <vfx.noaht@gmail.com>
2024-01-22 07:11:52 -0600

    Add url pattern for courselibrary app, add test view for courselibrary

--- golftracker/courselibrary/views.py
+++ golftracker/courselibrary/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Course
+
+def CourseListView(request):
+    courses = Course.objects.all()
+    context = {"courses": courses}
+    return render(request, 'courselibrary/courselibrary.html', context)
+
